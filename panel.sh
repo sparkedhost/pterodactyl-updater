5c9fdea9e2e80275f9b379f0bb5af5e135c703d2
harry.w <harry.w@sparked.support>
2021-11-07 18:22:59 +0000

    Basic Panel Update Script

---
+++ panel.sh
@@ -0,0 +1,13 @@
+PATH_TO_INSTALLATION=/var/www/pterodactyl
+
+cd $PATH_TO_INSTALLATION
+php artisan down
+curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+chmod -R 755 storage/* bootstrap/cache
+composer install --no-dev --optimize-autoloader
+php artisan view:clear
+php artisan config:clear
+php artisan migrate --seed --force
+chown -R www-data:www-data /var/www/pterodactyl/*
+php artisan queue:restart
+php artisan up
