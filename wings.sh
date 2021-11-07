2b3dc15c5da150cf1f59b75545161478059c9ca9
harry.w <harry.w@sparked.support>
2021-11-07 18:26:29 +0000

    Display progress

--- wings.sh
+++ wings.sh
@@ -1,5 +1,8 @@
 PATH_TO_INSTALLATION=/usr/local/bin/wings
 
+echo "Downloading latest Wings binary.."
 curl -L -o $PATH_TO_INSTALLATION https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_amd64
+echo "Applying permissions.."
 chmod u+x $PATH_TO_INSTALLATION
+echo "Restarting Wings.."
 systemctl restart wings
