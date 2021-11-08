078c9f0b2ee79bba97c1c4f7fbce723d8b01fa7f
hwalker928 <harry@harryw.link>
2021-11-08 19:31:41 +0000

    Extra checks & curl

--- install.sh
+++ install.sh
@@ -11,10 +11,20 @@ if [ -d /srv/updater ]; then
 fi
 if [ -d /var/www/pterodactyl ]; then
     echo "Pterodactyl Panel Installation detected."
+    if [ ! -d /srv/updater ]; then
+      echo "Creating folder for scripts."
+      mkdir /srv/updater
+    fi
+    mkdir /srv/updater
+    curl -o /srv/updater/panel.sh https://raw.githubusercontent.com/SparkedHost/Pterodactyl-Updater/main/panel.sh
 fi
 if [ -d /var/lib/pterodactyl ]; then
     echo "Pterodactyl Wings Installation detected."
+    if [ ! -d /srv/updater ]; then
+      echo "Creating folder for scripts."
+      mkdir /srv/updater
+    fi
+    curl -o /srv/updater/wings.sh https://raw.githubusercontent.com/SparkedHost/Pterodactyl-Updater/main/wings.sh
 fi
-mkdir /srv/updater
-curl -o /srv/updater/panel.sh https://raw.githubusercontent.com/SparkedHost/Pterodactyl-Updater/main/panel.sh
 echo ""
+exit
