a1bfd0e8021dfce4446ca9bbc988725e6257b433
harry.w <harry.w@sparked.support>
2021-11-08 19:23:55 +0000

    Add basic installer file

---
+++ install.sh
@@ -0,0 +1,19 @@
+clear
+echo ""
+echo "Sparked Host Pterodactyl Updater"
+echo "Developed by harry.w@sparked.support"
+echo ""
+echo "Detecting existing installation.."
+if [ -d /srv/updater ]; then
+    echo "Auto update script already installed."
+    exit
+fi
+if [ -d /var/www/pterodactyl ]; then
+    echo "Pterodactyl Panel Installation detected."
+fi
+if [ -d /var/lib/pterodactyl ]; then
+    echo "Pterodactyl Wings Installation detected."
+fi
+mkdir /srv/updater
+curl -o /srv/updater/panel.sh https://raw.githubusercontent.com/SparkedHost/Pterodactyl-Updater/main/panel.sh
+echo ""
