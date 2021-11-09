7428b6c1d797d0547f0284c1a7f5e00de302d16e
hwalker928 <harry@harryw.link>
2021-11-09 18:21:58 +0000

    Checks for needed packages

--- install.sh
+++ install.sh
@@ -1,8 +1,30 @@
+quit() {
+  echo ""
+  exit
+}
+
 clear
 echo ""
 echo "Sparked Host Pterodactyl Updater"
 echo "Developed by harry.w@sparked.support"
 echo ""
+echo "Checking dependencies.."
+
+if [ $EUID -ne 0 ]; then
+  echo "Please run the command with sudo."
+  quit
+fi
+
+if [ ! command -v apt-get &> /dev/null ]; then
+  echo "This script only works on Debian-based systems."
+  quit
+fi
+
+if [ ! command -v curl &> /dev/null ]; then
+  echo "Installing curl.."
+  apt-get update
+  apt-get install curl
+fi
 echo "Detecting existing installation.."
 if [ -d /srv/updater ]; then
     echo "Auto update script already installed."
@@ -12,11 +34,12 @@ fi
 if [ -d /var/www/pterodactyl ]; then
     echo "Pterodactyl Panel Installation detected."
     if [ ! -d /srv/updater ]; then
-      echo "Creating folder for scripts."
+      echo "Creating folder for scripts.."
       mkdir /srv/updater
     fi
-    mkdir /srv/updater
+    echo "Downloading Panel update script.."
     curl -o /srv/updater/panel.sh https://raw.githubusercontent.com/SparkedHost/Pterodactyl-Updater/main/panel.sh
+    echo "Adding Panel update script to crontab.."
     crontab -l | {
       cat
       echo "0 1 */2 * * sh /srv/updater/panel.sh >> /dev/null 2>&1"
@@ -28,11 +51,12 @@ if [ -d /var/lib/pterodactyl ]; then
       echo "Creating folder for scripts."
       mkdir /srv/updater
     fi
+    echo "Downloading Wings update script.."
     curl -o /srv/updater/wings.sh https://raw.githubusercontent.com/SparkedHost/Pterodactyl-Updater/main/wings.sh
+    echo "Adding Wings update script to crontab.."
     crontab -l | {
       cat
       echo "0 1 */2 * * sh /srv/updater/wings.sh >> /dev/null 2>&1"
     } | crontab -
 fi
-echo ""
-exit
+quit
