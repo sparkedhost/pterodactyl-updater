5300bbccad7df69eeb8649a18a92727132fe15fa
Bad <87938689+yesBad@users.noreply.github.com>
2021-11-13 21:57:50 +0200

    Simplified panel.sh
    This will work if they have 1.3.2 or higher which everyone should have :D

--- panel.sh
+++ panel.sh
@@ -12,22 +12,4 @@ then
 fi
 
 export COMPOSER_ALLOW_SUPERUSER=1;
-echo "Setting Panel to maintenance mode.."
-php artisan down
-echo "Downloading latest Panel files.."
-curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
-echo "Setting permissions.."
-chmod -R 755 storage/* bootstrap/cache
-echo "Installing Composer dependencies.."
-composer install --no-dev --optimize-autoloader
-echo "Clearing view and config cache.."
-php artisan view:clear
-php artisan config:clear
-echo "Running migrations.."
-php artisan migrate --seed --force
-echo "Setting permissions.."
-chown -R www-data:www-data /var/www/pterodactyl/*
-echo "Restarting queue worker.."
-php artisan queue:restart
-echo "Restoring Panel functionality.."
-php artisan up
+cd /var/www/pterodactyl && php artisan p:upgrade -n
